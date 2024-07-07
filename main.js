945b0322bd4faff1f558ee1c1b57dcca49dbf64c
Bruno Cocco <Brunoe.cocco@gmail.com>
2024-07-07 19:50:08 +0300

    iniciando proyecto

---
+++ main.js
@@ -0,0 +1,40 @@
+/* Email sender */
+const EMAIL = [];
+
+class CorreoEmail {
+    constructor(destino,correo,textoEmail){
+        this.destino = destino,
+        this.correo = correo,
+        this.textoEmail = textoEmail
+    }
+}
+
+const enviar = document.getElementById("enviar");
+
+const EnviarCorreo = () =>{
+
+    let destino = document.getElementById("destinatario").value;
+    let correo = document.getElementById("email").value;
+    let textoEmail = document.getElementById("textoEmail").value;
+    console.log(destino)
+    console.log(correo)
+    console.log(textoEmail)
+
+    let nuevoitem = new CorreoEmail(destino,correo,textoEmail)
+    EMAIL.push(nuevoitem)
+    console.log(EMAIL)
+
+    localStorage.setItem("dato", JSON.stringify(EMAIL))
+
+    chequeoStorage();
+    
+}
+
+
+function chequeoStorage() {
+    let datoguardado = JSON.parse(localStorage.getItem("dato"));
+    console.log(datoguardado)
+}
+
+
+enviar.addEventListener("click", EnviarCorreo)
