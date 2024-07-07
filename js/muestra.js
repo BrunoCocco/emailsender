7c5e8fb86552421b7d5f2d7509f1e388d520b084
Bruno Cocco <Brunoe.cocco@gmail.com>
2024-07-07 20:38:05 +0300

    generamos la card que se imprime en pantalla

--- js/muestra.js
+++ js/muestra.js
@@ -1,10 +1,34 @@
-let datoguardado = JSON.parse(localStorage.getItem("dato"));
-console.log(datoguardado)
+const mostrarTarjetas = () => {
 
-const STORAGE = []
-STORAGE.push(datoguardado);
-console.log(STORAGE)
+    let datoguardado = JSON.parse(localStorage.getItem("dato"));
 
-let div = document.getElementById("imprimirMail")
-console.log(div)
+    const STORAGE = []
+    STORAGE.push(datoguardado);
+    console.log(STORAGE)
 
+    let div = document.getElementById("cards-container")
+    console.log(div)
+
+    // Limpiar el contenedor antes de agregar nuevas tarjetas
+    div.innerHTML = '';
+
+    // Recorrer el array de objetos y crear una tarjeta para cada uno
+    datoguardado.forEach(email => {
+        // Crear una tarjeta en forma de string HTML
+        let cardHTML = `
+            <div>
+                <div>
+                    <h2>Destinatario: ${email.destino}</h2>
+                    <h4>Correo: ${email.correo}</h4>
+                    <h2>Texto del Email: ${email.textoEmail}</h2>
+                </div>
+            </div>
+        `;
+
+        // Agregar la tarjeta al contenedor
+        div.innerHTML += cardHTML;
+    });
+
+}
+
+mostrarTarjetas();
