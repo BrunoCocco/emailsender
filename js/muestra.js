77f6bcb504b72fd18bfe7132d9b72b5140fffa55
Bruno Cocco <Brunoe.cocco@gmail.com>
2024-07-07 20:21:40 +0300

    actualizando

---
+++ js/muestra.js
@@ -0,0 +1,10 @@
+let datoguardado = JSON.parse(localStorage.getItem("dato"));
+console.log(datoguardado)
+
+const STORAGE = []
+STORAGE.push(datoguardado);
+console.log(STORAGE)
+
+let div = document.getElementById("imprimirMail")
+console.log(div)
+
